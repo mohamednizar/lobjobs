c9530830e6e87454f980e44c6b839d4dd83b778d
nizar <nizarucsc@gmail.com>
2016-08-06 13:09:37 +0530

    fix the isses

--- config/db.class.php
+++ config/db.class.php
@@ -4,20 +4,23 @@ $database = "u426162963_lob";
 $username = "u426162963_admin";
 $password = "6pgHDcoHxo";
 
-$mysqli = new mysqli($server, $username, $password, $database);
-/* check connection */
-if (mysqli_connect_errno()) {
-    printf("Connect failed: %s\n", mysqli_connect_error());
-    exit();
-}
+// $mysqli = new mysqli($server, $username, $password, $database);
+// /* check connection */
+// if (mysqli_connect_errno()) {
+//     printf("Connect failed: %s\n", mysqli_connect_error());
+//     exit();
+// }
+
+$mysqlConnection = @mysql_connect($server,$username,$password);
+
 
 // $mysqlConnection = mysql_connect($server, $username, $password);
-// if (!$mysqlConnection)
-// {
-//   echo "Please try later.";
-// }
-// else{
-// mysql_select_db($database, $mysqlConnection);
+if (!$mysqlConnection)
+{
+  echo "Please try later.";
+}
+else{
+mysql_select_db($database);
 
-// }
+}
 ?>
